096713d0b9f0cb84f67021a8e74415375f5e8ab4
jayasuryamk <jayasuryamani@gmail.com>
2024-06-02 23:12:19 +0530

    delete code at js file

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
-
-const btnNav = document.querySelector(".mobile-nav");
-const header = document.querySelector(".header");
-btnNav.addEventListener("click", function () {
-  header.classList.toggle("nav-open");
-});
